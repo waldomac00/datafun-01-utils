bc4b26ab19cc3db122732436346a32192bd46469
Michael J Moore <waldomac00@live.com>
2025-08-27 21:55:51 -0500

    update header information

--- utils_michaeljmoore.py
+++ utils_michaeljmoore.py
@@ -1,5 +1,5 @@
 """
-File: utils_case.py
+File: utils_michaeljmoore.py
 
 Purpose: Reusable header/tagline module for analytics projects.
 
@@ -10,11 +10,8 @@ A short, first-week module to demonstrate key skills:
 - expose a function named get_tagline() that can be imported into other modules
 - run this file as a script via main() using the if __name__ == '__main__' pattern
 
-Author: Denise Case
+Author: Michael J Moore
 
-TODO: Replace 'lastname' in the filename with your own last (or unique) name.
-TODO: Update the file name and author in this opening docstring.
-TODO: Remove each TODO line AFTER you successfully complete the item.
 """
 
 #####################################
